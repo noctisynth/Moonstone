860737e0d03f9df320f78504d97898390f195be2
苏向夜 <fu050409@163.com>
2024-02-07 00:15:37 +0800

    feat(UI): Improve messages display

--- src/components/Main/Messages.vue
+++ src/components/Main/Messages.vue
@@ -12,10 +12,12 @@ const messages = ref([
         items: [
             {
                 text: '我要跑路',
+                status: 'verified',
                 timestamp: '2024/2/6 19:54',
             },
             {
                 text: '跑路跑路',
+                status: 'verified',
                 timestamp: '2024/2/6 19:54',
             },
         ]
@@ -28,6 +30,22 @@ const messages = ref([
         items: [
             {
                 text: '不许跑',
+                status: 'sending',
+                timestamp: '2024/2/6 19:54',
+            },
+            {
+                text: '#敲',
+                status: 'failed',
+                timestamp: '2024/2/6 19:54',
+            },
+            {
+                text: '#捉',
+                status: 'reached',
+                timestamp: '2024/2/6 19:54',
+            },
+            {
+                text: '谴责！',
+                status: 'check',
                 timestamp: '2024/2/6 19:54',
             },
         ]
@@ -37,17 +55,42 @@ const messages = ref([
 
 <template>
     <div class="card w-full flex justify-content-center h-full">
-        <Listbox v-model="selectedMessage" :options="messages" optionLabel="label" optionGroupLabel="label"
-            optionGroupChildren="items" class="w-full h-full">
+        <Listbox v-model="selectedMessage" :options="messages" optionGroupLabel=" " optionGroupChildren="items"
+            class="w-full h-full">
             <template #optiongroup="slot">
                 <div class="flex align-items-center">
                     <div>{{ slot.option.user.nickname }}</div>
                 </div>
             </template>
             <template #option="slot">
-                <!-- <div class="flex align-items-center"> -->
-                <Button v-tooltip.bottom="slot.option.timestamp" plain text>{{ slot.option.text }}</Button>
-                <!-- </div> -->
+                <div v-tooltip.bottom="slot.option.timestamp">
+                    <div v-if="slot.option.status === 'sending'">
+                        <Button class="text-white" :label="slot.option.text" badge=" "
+                            badgeClass="bg-primary flex justify-content-center align-items-center pi pi-spin pi-spinner"
+                            plain text>
+                        </Button>
+                    </div>
+                    <div v-else-if="slot.option.status === 'failed'">
+                        <Button class="text-white" :label="slot.option.text" badge=" "
+                            badgeClass="bg-red-900 flex justify-content-center align-items-center pi pi-times" plain text>
+                        </Button>
+                    </div>
+                    <div v-else-if="slot.option.status === 'verified'">
+                        <Button class="text-white" :label="slot.option.text" badge=" "
+                            badgeClass="bg-green-900 flex justify-content-center align-items-center pi pi-verified" plain
+                            text>
+                        </Button>
+                    </div>
+                    <div v-else-if="slot.option.status === 'check'">
+                        <Button class="text-white" :label="slot.option.text" badge=" "
+                            badgeClass="bg-green-900 flex justify-content-center align-items-center pi pi-check" plain text>
+                        </Button>
+                    </div>
+                    <div v-else>
+                        <Button class="text-white" :label="slot.option.text" plain text>
+                        </Button>
+                    </div>
+                </div>
             </template>
         </Listbox>
     </div>
