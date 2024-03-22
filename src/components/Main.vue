4adc7e0bbb9f16b11c89bd0c0052315733e7d825
苏向夜 <fu050409@163.com>
2024-03-22 19:21:22 +0800

    feat(main): refactor ui for main window

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,99 +1,94 @@
 <script setup lang="ts">
-// import SideToolBar from "./Main/SideToolBar.vue"
-// import Sessions from "./Main/Sessions.vue"
-// import EditorPanel from './Main/EditorPanel.vue';
-// import TopBar from "./Main/TopBar.vue";
-// import Messages from "./Main/Messages.vue";
-// import Splitter from "primevue/splitter";
-// import SplitterPanel from "primevue/splitterpanel";
+import PanelMenu from 'primevue/panelmenu';
+import Button from 'primevue/button';
+import Avatar from 'primevue/avatar';
+import Divider from 'primevue/divider';
+import Dialog from 'primevue/dialog';
+import InputSwitch from 'primevue/inputswitch';
+import InputText from 'primevue/inputtext';
+import Toast from 'primevue/toast';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import { ref } from 'vue';
+import { useThemeStore } from '../stores/theme';
+import { usePrimeVue } from 'primevue/config';
+import { useToast } from 'primevue/usetoast';
 
-// import { ref } from 'vue';
+const items = ref([
+    {
+        label: '信道',
+        icon: 'pi pi-inbox',
+        items: [
 
-// let session = ref()
-// let messages = ref([
-//     {
-//         user: {
-//             nickname: '雪花'
-//         },
-//         sequence: '',
-//         timestamp: '2024/2/6 19:54',
-//         items: [
-//             {
-//                 text: '我要跑路',
-//                 status: 'verified',
-//                 timestamp: '2024/2/6 19:54',
-//             },
-//             {
-//                 text: '跑路跑路',
-//                 status: 'verified',
-//                 timestamp: '2024/2/6 19:54',
-//             },
-//         ]
-//     },
-//     {
-//         user: {
-//             nickname: '苏向夜'
-//         },
-//         sequence: '',
-//         items: [
-//             {
-//                 text: '不许跑',
-//                 status: 'sending',
-//                 timestamp: '2024/2/6 19:54',
-//             },
-//             {
-//                 text: '#敲',
-//                 status: 'failed',
-//                 timestamp: '2024/2/6 19:54',
-//             },
-//             {
-//                 text: '#捉',
-//                 status: 'reached',
-//                 timestamp: '2024/2/6 19:54',
-//             },
-//             {
-//                 text: '谴责！',
-//                 status: 'check',
-//                 timestamp: '2024/2/6 19:54',
-//             },
-//         ]
-//     },
-// ]);
+        ]
+    },
+    {
+        label: '群组',
+        icon: 'pi pi-users',
+        items: [
 
-// function sessionChanged(node: any) {
-//     session.value = node;
-// }
+        ]
+    }
+])
+
+const showDialog = ref<boolean>(false)
+const themestore = useThemeStore()
+const PrimeVue = usePrimeVue()
+const toast = useToast()
 </script>
 
 <template>
-    <!-- <Splitter class="h-full" style="border: none;" :pt="{
-        gutter: { style: 'width: 3px; cursor: ew-resize' },
-        gutterHandler: { style: 'display: none' }
-    }">
-        <SplitterPanel class="flex align-items-center justify-content-center fixed-width-panel" :size="15" :minSize="15"
-            :maxSize="15">
-            <SideToolBar />
-        </SplitterPanel>
-        <SplitterPanel class="flex align-items-center justify-content-center" :size="20">
-            <Sessions @session-changed="sessionChanged" />
-        </SplitterPanel>
-        <SplitterPanel :size="65">
-            <Splitter v-if="session" layout="vertical" :pt="{
-        gutter: { style: 'height: 3px; cursor: ns-resize' },
-        gutterHandler: { style: 'display: none' }
-    }">
-                <SplitterPanel class="flex align-items-center justify-content-center" :size="10">
-                    <TopBar v-model="session" />
-                </SplitterPanel>
-                <SplitterPanel :size="65" style="overflow-y: auto;">
-                    <Messages v-model="messages" />
-                </SplitterPanel>
-                <SplitterPanel :size="25" style="overflow: hidden;">
-                    <EditorPanel v-model:session="session" v-model:messages="messages" />
-                </SplitterPanel>
-            </Splitter>
-        </SplitterPanel>
-    </Splitter> -->
+    <div class="w-full h-full">
+        <Toast></Toast>
+        <div class="flex flex-row h-full">
+            <div
+                class="flex flex-column justify-content-between align-items-center p-3 bg-surface-300 border-right-2 border-200">
+                <div class="flex flex-column justify-content-center align-items-center">
+                    <Avatar :label="'苏'"></Avatar>
+                    <Divider layout="horizontal"></Divider>
+                    <Button @click="toast.add({
+                        'summary': '暂未开放', 'detail': '插件系统暂未开放，暂不支持增删插件！', 'life': 3000, 'severity': 'warn'
+                    })" icon="pi pi-plus" plain text></Button>
+                </div>
+                <div class="flex flex-column gap-3">
+                    <Button @click="showDialog = true" icon="pi pi-cog" outlined></Button>
+                    <Dialog v-model:visible="showDialog" modal class="max-w-full" style="width: 30rem;">
+                        <template #header>
+                            <div class="inline-flex align-items-center justify-content-center gap-2">
+                                <span class="font-bold white-space-nowrap">设置</span>
+                            </div>
+                        </template>
+                        <div class="flex flex-column justify-content-center align-items-center">
+                            <div class="flex flex-column w-full p-2">
+                                <div class="inline-flex justify-content-between">
+                                    <div><span>风格</span></div>
+                                    <InputSwitch v-model="themestore.dark" @click="themestore.changeTheme(PrimeVue)">
+                                    </InputSwitch>
+                                </div>
+                            </div>
+                        </div>
+                    </Dialog>
+                </div>
+            </div>
+            <div class="w-full p-2">
+                <div class="w-full h-full flex flex-column gap-3 p-1">
+                    <div class="inline-flex justify-content-between gap-3 max-w-full">
+                        <IconField iconPosition="left">
+                            <InputIcon class="pi pi-search"></InputIcon>
+                            <InputText placeholder="搜索" class="w-full"></InputText>
+                        </IconField>
+                        <Button icon="pi pi-plus" size="small" outlined></Button>
+                    </div>
+                    <Divider class="m-0"></Divider>
+                    <PanelMenu :model="items" multiple class="w-full"></PanelMenu>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+:deep(.p-divider.p-divider-horizontal:before) {
+    border-top-width: 2px;
+}
+</style>
