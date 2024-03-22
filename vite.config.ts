37b387cded3c9d828907f31020ccd2810d137b81
苏向夜 <fu050409@163.com>
2024-03-22 16:16:26 +0800

    chore(vite): update config for vite to tauri v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";;
+import vue from "@vitejs/plugin-vue";
+import { internalIpV4 } from "internal-ip";
+
+// @ts-expect-error process is a nodejs global
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -13,5 +17,17 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-  }
+    host: mobile ? "0.0.0.0" : false,
+    hmr: mobile
+      ? {
+          protocol: "ws",
+          host: await internalIpV4(),
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 }));
