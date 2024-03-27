c467c3c68ed49a25ecf56a3de94bf0c7ceca8a39
苏向夜 <fu050409@163.com>
2024-03-27 22:18:17 +0800

    chore(version): bump version to 0.2.0-alpha.11

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "0.2.0-alpha.10"
+        versionName = "0.2.0-alpha.11"
     }
     signingConfigs {
         create("release") {
