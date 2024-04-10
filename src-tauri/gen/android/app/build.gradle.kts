8bc6e97e577da6779701fb00dfe9654e12eb5006
苏向夜 <fu050409@163.com>
2024-04-10 20:56:25 +0800

    chore(version): bump version to v0.2.0-nightly.17

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "0.2.0-nightly.16"
+        versionName = "0.2.0-nightly.17"
     }
     signingConfigs {
         create("release") {
