4188862bd16934dc3a382d79e4aa2b641ec1a089
苏向夜 <fu050409@163.com>
2024-04-10 16:20:40 +0800

    refactor(anyhow): use anyhow instead of std

--- src-tauri/src/api/account.rs
+++ src-tauri/src/api/account.rs
@@ -1,3 +1,4 @@
+use anyhow::{Error, Result};
 use oblivion::api;
 use serde_json::{json, Value};
 
@@ -8,7 +9,7 @@ pub async fn login(
     identity: &str,
     password: &str,
     unique_id: &str,
-) -> Result<String, Exception> {
+) -> Result<String> {
     match api::post(
         format!("{server}/session/new").as_str(),
         json!({"identity": identity, "password": password, "unique_id": unique_id}),
@@ -23,42 +24,42 @@ pub async fn login(
                         match result["status"].as_bool() {
                             Some(status) => {
                                 if !status {
-                                    return Err(Exception::BadResponse {
+                                    return Err(Error::from(Exception::BadResponse {
                                         detail: res.text().unwrap(),
-                                    });
+                                    }));
                                 }
                             }
                             None => {
-                                return Err(Exception::BadResponse {
+                                return Err(Error::from(Exception::BadResponse {
                                     detail: res.text().unwrap(),
-                                });
+                                }));
                             }
                         };
                         match result["session_key"].as_str() {
                             Some(session_key) => Ok(session_key.to_string()),
                             None => {
-                                return Err(Exception::BadResponse {
+                                return Err(Error::from(Exception::BadResponse {
                                     detail: res.text().unwrap(),
-                                });
+                                }));
                             }
                         }
                     }
-                    Err(_) => Err(Exception::BadResponse {
+                    Err(_) => Err(Error::from(Exception::BadResponse {
                         detail: res.text().unwrap(),
-                    }),
+                    })),
                 }
             } else {
-                Err(Exception::AuthenticationFailed {
+                Err(Error::from(Exception::AuthenticationFailed {
                     identity: identity.to_string(),
                     password: password.to_string(),
-                })
+                }))
             }
         }
-        Err(error) => Err(Exception::ConnectionError { error }),
+        Err(error) => Err(Error::from(Exception::ConnectionError { error })),
     }
 }
 
-pub async fn profile(node: &str, session_key: &str) -> Result<Value, Exception> {
+pub async fn profile(node: &str, session_key: &str) -> Result<Value> {
     let mut res = match api::post(
         format!("{node}/account/profile").as_str(),
         json!({"session_key": session_key}),
@@ -68,15 +69,15 @@ pub async fn profile(node: &str, session_key: &str) -> Result<Value, Exception> 
     {
         Ok(res) => res,
         Err(error) => {
-            return Err(Exception::ConnectionError { error: error });
+            return Err(Error::from(Exception::ConnectionError { error }));
         }
     };
 
     match res.json() {
         Ok(json) => Ok(json),
-        Err(error) => Err(Exception::BadResponse {
+        Err(error) => Err(Error::from(Exception::BadResponse {
             detail: error.to_string(),
-        }),
+        })),
     }
 }
 
@@ -86,7 +87,7 @@ pub(crate) async fn register(
     username: &str,
     nickname: &str,
     password: &str,
-) -> Result<Value, Exception> {
+) -> Result<Value> {
     let mut res = match api::post(
         format!("{node}/account/new").as_str(),
         json!({"tuta_mail": tuta_mail, "username": username, "nickname": nickname, "password": password}),
@@ -96,7 +97,7 @@ pub(crate) async fn register(
     {
         Ok(res) => res,
         Err(error) => {
-            return Err(Exception::ConnectionError { error: error });
+            return Err(Error::from(Exception::ConnectionError { error }));
         }
     };
 
@@ -110,13 +111,13 @@ pub(crate) async fn register(
             if status {
                 Ok(json)
             } else {
-                Err(Exception::RequestFailed {
+                Err(Error::from(Exception::RequestFailed {
                     detail: json["msg"].as_str().unwrap().to_string(),
-                })
+                }))
             }
         }
-        Err(error) => Err(Exception::BadResponse {
+        Err(error) => Err(Error::from(Exception::BadResponse {
             detail: error.to_string(),
-        }),
+        })),
     }
 }
